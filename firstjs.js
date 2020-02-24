dfbd09883fe30d9bd89bcb2bab9dc2897c76ee0b
N150709 <59652481+N150709@users.noreply.github.com>
2020-02-24 12:11:41 +0530

    Update firstjs.js

--- firstjs.js
+++ firstjs.js
@@ -1,1 +1,13 @@
-document.body.textContent = "HELLO WORLD!";
+import ws from 'ws';
+
+const wsServer = new ws.Server({port:8080});
+
+wsServer.on('connection',ws => {
+	ws.on('message',message => {
+		for(const client of wsServer.clients){
+			if(client.readyState === ws.OPEN){
+				client.send(message);
+			}
+		}
+	});
+});
