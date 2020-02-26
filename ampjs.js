f7b3b36efe7d643c2ad35645f5928685904472cf
N150709 <59652481+N150709@users.noreply.github.com>
2020-02-26 16:39:28 +0530

    Update ampjs.js

--- ampjs.js
+++ ampjs.js
@@ -1,6 +1,1 @@
-const button = document.querySelector('#hello2');
-    button.addEventListener('click', () => {
-      const h1 = document.createElement('h1');
-      h1.textContent = 'Hello World 2!';
-      document.body.appendChild(h1);
-    });
+const button = document.querySelector('#hello2');button.addEventListener('click', () => {const h1 = document.createElement('h1');h1.textContent = 'Hello World 2!';document.body.appendChild(h1);});
