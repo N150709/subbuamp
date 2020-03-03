3b259e16122e9a651bf7bf774b0e3595d7a10f2e
N150709 <59652481+N150709@users.noreply.github.com>
2020-03-03 17:27:11 +0530

    Update firstjs.js

--- firstjs.js
+++ firstjs.js
@@ -1,13 +1,1 @@
-import ws from 'ws';
-
-const wsServer = new ws.Server({port:8080});
-
-wsServer.on('connection',ws => {
-	ws.on('message',message => {
-		for(const client of wsServer.clients){
-			if(client.readyState === ws.OPEN){
-				client.send(message);
-			}
-		}
-	});
-});
+console.log('hello')
